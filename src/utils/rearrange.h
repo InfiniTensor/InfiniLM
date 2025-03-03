ebff82e374a63862c6d0d5f330d2d60c6b105147
YdrMaster <ydrml@hotmail.com>
2025-03-03 14:40:22 +0800

    issue/78/feat: 实现 rearrange
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/utils/rearrange.h
+++ src/utils/rearrange.h
@@ -1,13 +1,19 @@
-#ifndef INFINIUTILS_REARRANGE_H
-#define INFINIUTILS_REARRANGE_H
+#ifndef __INFINIUTILS_REARRANGE_H__
+#define __INFINIUTILS_REARRANGE_H__
+
 #include <stddef.h>
 
-void rearrange(void *dst,
-               const void *src,
-               const size_t *shape,
-               const ptrdiff_t *dst_strides,
-               const ptrdiff_t *src_strides,
-               const size_t ndim,
-               size_t element_size);
+namespace utils {
 
-#endif // INFINIUTILS_REARRANGE_H
+void rearrange(
+    void *dst,
+    const void *src,
+    const size_t *shape,
+    const ptrdiff_t *dst_strides,
+    const ptrdiff_t *src_strides,
+    size_t ndim,
+    size_t element_size);
+
+} // namespace utils
+
+#endif // __INFINIUTILS_REARRANGE_H__
