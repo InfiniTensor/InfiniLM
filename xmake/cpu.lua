e3f118a15b6b6d940d2b5349643ee526d8d4d991
zhangyue <zhangyue@qiyuanlab.com>
2025-03-17 13:00:14 +0800

    issue/87: undo gomp

--- xmake/cpu.lua
+++ xmake/cpu.lua
@@ -17,7 +17,6 @@ target("infiniop-cpu")
         end
     end
 
-    add_links("gomp")
     set_languages("cxx17")
     add_files("../src/infiniop/devices/cpu/*.cc", "../src/infiniop/ops/*/cpu/*.cc")
 
