47027899524b8bb20a6c6d7f2a5df1d2da85f2af
PanZezhong <panzezhong@qiyuanlab.com>
2025-04-03 18:01:32 +0800

    issue/134/refactor 修改set env

--- scripts/install.py
+++ scripts/install.py
@@ -2,33 +2,10 @@ import os
 import subprocess
 import platform
 import sys
+from set_env import set_env
 
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 os.chdir(PROJECT_DIR)
-
-
-def set_env():
-    if os.environ.get("INFINI_ROOT", "") == "":
-        os.environ["INFINI_ROOT"] = os.path.expanduser("~/.infini")
-
-    if platform.system() == "Windows":
-        new_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/bin")
-        if new_path not in os.environ.get("PATH", ""):
-            os.environ["PATH"] = f"{new_path};{os.environ.get('PATH', '')}"
-
-    elif platform.system() == "Linux":
-        new_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/bin")
-        if new_path not in os.environ.get("PATH", ""):
-            os.environ["PATH"] = f"{new_path}:{os.environ.get('PATH', '')}"
-
-        new_lib_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/lib")
-        if new_lib_path not in os.environ.get("LD_LIBRARY_PATH", ""):
-            os.environ["LD_LIBRARY_PATH"] = (
-                f"{new_lib_path}:{os.environ.get('LD_LIBRARY_PATH', '')}"
-            )
-    else:
-        raise RuntimeError("Unsupported platform.")
-
 
 def run_cmd(cmd):
     subprocess.run(cmd, text=True, encoding="utf-8", check=True, shell=True)
--- scripts/python_test.py
+++ scripts/python_test.py
@@ -1,35 +1,12 @@
 import os
 import subprocess
-import platform
+from set_env import set_env
 import sys
 
 PROJECT_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..", "test", "infiniop")
 )
 os.chdir(PROJECT_DIR)
-
-
-def set_env():
-    if os.environ.get("INFINI_ROOT", "") == "":
-        os.environ["INFINI_ROOT"] = os.path.expanduser("~/.infini")
-
-    if platform.system() == "Windows":
-        new_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/bin")
-        if new_path not in os.environ.get("PATH", ""):
-            os.environ["PATH"] = f"{new_path};{os.environ.get('PATH', '')}"
-
-    elif platform.system() == "Linux":
-        new_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/bin")
-        if new_path not in os.environ.get("PATH", ""):
-            os.environ["PATH"] = f"{new_path}:{os.environ.get('PATH', '')}"
-
-        new_lib_path = os.path.expanduser(os.environ.get("INFINI_ROOT") + "/lib")
-        if new_lib_path not in os.environ.get("LD_LIBRARY_PATH", ""):
-            os.environ["LD_LIBRARY_PATH"] = (
-                f"{new_lib_path}:{os.environ.get('LD_LIBRARY_PATH', '')}"
-            )
-    else:
-        raise RuntimeError("Unsupported platform.")
 
 
 def run_tests(args):
