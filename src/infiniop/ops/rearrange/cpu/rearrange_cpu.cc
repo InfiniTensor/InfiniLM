PanZezhong1725/InfiniCore

issue/131: rearrange 算子 - CUDA 同时修复 CPU rearrange 算子内存泄漏问题

src/infiniop/ops/rearrange/cpu/rearrange_cpu.cc
@@ -15,20 +15,17 @@ infiniStatus_t Descriptor::create(
     auto handle = reinterpret_cast<device::cpu::Handle *>(handle_);
     auto dtype = y_desc->dtype();
     auto ndim = y_desc->ndim();
-    auto shape = y_desc->shape().data();
 
+    auto shape = y_desc->shape();
     CHECK_API_OR(x_desc->dtype(), dtype, return INFINI_STATUS_BAD_TENSOR_DTYPE);
     CHECK_API_OR(x_desc->ndim(), ndim, return INFINI_STATUS_BAD_TENSOR_SHAPE);
-
     for (size_t i = 0; i < ndim; ++i) {
         CHECK_API_OR(x_desc->shape()[i], shape[i], return INFINI_STATUS_BAD_TENSOR_SHAPE);
     }
-
-    auto dst_strides = y_desc->strides().data();
-    auto src_strides = x_desc->strides().data();
+    auto dst_strides = y_desc->strides();
+    auto src_strides = x_desc->strides();
     auto element_size = infiniSizeOf(dtype);
-    auto meta = utils::RearrangeMeta::create(shape, dst_strides, src_strides, ndim, element_size);
-
+    auto meta = utils::RearrangeMeta::create(shape.data(), dst_strides.data(), src_strides.data(), ndim, element_size);
     if (!meta) {
         return INFINI_STATUS_BAD_TENSOR_STRIDES;
     }

src/utils/rearrange.cc
@@ -136,4 +136,73 @@ void rearrange(
     }
 }
 
+std::optional<RearrangeMeta> RearrangeMeta::distribute_unit(const std::vector<size_t> &candidates) const {
+    // 获取当前的unit大小
+    size_t current_unit = _meta[0];
+
+    // 寻找满足条件的unit值：当前unit能被其整除
+    size_t new_unit = 0;
+    for (size_t candidate : candidates) {
+        if (current_unit % candidate == 0) {
+            new_unit = candidate;
+            break;
+        }
+    }
+
+    // 如果没找到合适的值，返回错误
+    if (new_unit == 0) {
+        return std::nullopt;
+    }
+
+    // 如果找到的值就是当前unit，返回自身的副本
+    if (new_unit == current_unit) {
+        return RearrangeMeta(_meta);
+    }
+
+    // 获取当前维度
+    size_t ndim_value = this->ndim();
+
+    // 创建新的布局数组
+    std::vector<ptrdiff_t> layout(2 + (ndim_value + 1) * 3, 0);
+
+    // 设置新的unit值
+    layout[0] = new_unit;
+
+    // 计算扩展因子
+    ptrdiff_t extra = current_unit / new_unit;
+
+    // 计算步长指针的偏移量
+    ptrdiff_t idx_offset = 1;
+
+    // 在新布局中设置相应的指针
+    ptrdiff_t *new_idx = layout.data() + 1;
+    ptrdiff_t *new_dst = layout.data() + 2 + (ndim_value + 1);
+    ptrdiff_t *new_src = layout.data() + 2 + (ndim_value + 1) * 2;
+
+    // 复制并调整索引步长
+
+    // 索引步长需要重新计算
+    // 首先复制原来的索引步长
+    for (size_t i = 0; i < ndim_value + 1; ++i) {
+        new_idx[i] = _meta[idx_offset + i] * extra;
+    }
+
+    // 设置最后一个维度的步长为1
+    new_idx[ndim_value + 1] = 1;
+
+    // 复制目标步长数据，并添加新单元大小
+    for (size_t i = 0; i < ndim_value; ++i) {
+        new_dst[i] = dst_strides()[i];
+    }
+    new_dst[ndim_value] = new_unit;
+
+    // 复制源步长数据，并添加新单元大小
+    for (size_t i = 0; i < ndim_value; ++i) {
+        new_src[i] = src_strides()[i];
+    }
+    new_src[ndim_value] = new_unit;
+
+    return RearrangeMeta(layout);
+}
+
 } // namespace utils

src/utils/rearrange.h
@@ -28,6 +28,9 @@ public:
     const ptrdiff_t *src_strides() const;
 
     void launch(void *dst, const void *src) const;
+
+    // 拆分 unit 到更小的规模以利于并行
+    std::optional<RearrangeMeta> distribute_unit(const std::vector<size_t> &candidates) const;
 };
 
 void rearrange(

InfiniTensor/InfiniLM

src/infiniop/ops/rearrange/cpu/rearrange_cpu.cc
@@ -15,20 +15,18 @@ infiniStatus_t Descriptor::create(
     auto handle = reinterpret_cast<device::cpu::Handle *>(handle_);
     auto dtype = y_desc->dtype();
     auto ndim = y_desc->ndim();
-    auto shape = y_desc->shape().data();
 
-    CHECK_API_OR(x_desc->dtype(), dtype, return INFINI_STATUS_BAD_TENSOR_DTYPE);
-    CHECK_API_OR(x_desc->ndim(), ndim, return INFINI_STATUS_BAD_TENSOR_SHAPE);
+    auto y_shape = y_desc->shape();
+    auto x_shape = x_desc->shape();
+    CHECK_OR_RETURN(x_desc->dtype() == dtype, INFINI_STATUS_BAD_TENSOR_DTYPE);
+    CHECK_OR_RETURN(x_desc->ndim() == ndim, INFINI_STATUS_BAD_TENSOR_SHAPE);
+    CHECK_SAME_SHAPE(x_shape, y_shape);
 
-    for (size_t i = 0; i < ndim; ++i) {
-        CHECK_API_OR(x_desc->shape()[i], shape[i], return INFINI_STATUS_BAD_TENSOR_SHAPE);
-    }
-
-    auto dst_strides = y_desc->strides().data();
-    auto src_strides = x_desc->strides().data();
+    auto dst_strides = y_desc->strides();
+    auto src_strides = x_desc->strides();
     auto element_size = infiniSizeOf(dtype);
 
-    auto result = utils::RearrangeMeta::create(shape, dst_strides, src_strides, ndim, element_size);
+    auto result = utils::RearrangeMeta::create(y_shape.data(), dst_strides.data(), src_strides.data(), ndim, element_size);
     CHECK_RESULT(result);
 
     *desc_ptr = new Descriptor(

src/utils/rearrange.cc
@@ -138,4 +138,73 @@ void rearrange(
     }
 }
 
+utils::Result<RearrangeMeta> RearrangeMeta::distributeUnit(const std::vector<size_t> &candidates) const {
+    // 获取当前的unit大小
+    size_t current_unit = _meta[0];
+
+    // 寻找满足条件的unit值：当前unit能被其整除
+    size_t new_unit = 0;
+    for (size_t candidate : candidates) {
+        if (current_unit % candidate == 0) {
+            new_unit = candidate;
+            break;
+        }
+    }
+
+    // 如果没找到合适的值，返回错误
+    if (new_unit == 0) {
+        return INFINI_STATUS_BAD_PARAM;
+    }
+
+    // 如果找到的值就是当前unit，返回自身的副本
+    if (new_unit == current_unit) {
+        return Result<RearrangeMeta>(_meta);
+    }
+
+    // 获取当前维度
+    size_t ndim_value = this->ndim();
+
+    // 创建新的布局数组
+    std::vector<ptrdiff_t> layout(2 + (ndim_value + 1) * 3, 0);
+
+    // 设置新的unit值
+    layout[0] = new_unit;
+
+    // 计算扩展因子
+    ptrdiff_t extra = current_unit / new_unit;
+
+    // 计算步长指针的偏移量
+    ptrdiff_t idx_offset = 1;
+
+    // 在新布局中设置相应的指针
+    ptrdiff_t *new_idx = layout.data() + 1;
+    ptrdiff_t *new_dst = layout.data() + 2 + (ndim_value + 1);
+    ptrdiff_t *new_src = layout.data() + 2 + (ndim_value + 1) * 2;
+
+    // 复制并调整索引步长
+
+    // 索引步长需要重新计算
+    // 首先复制原来的索引步长
+    for (size_t i = 0; i < ndim_value + 1; ++i) {
+        new_idx[i] = _meta[idx_offset + i] * extra;
+    }
+
+    // 设置最后一个维度的步长为1
+    new_idx[ndim_value + 1] = 1;
+
+    // 复制目标步长数据，并添加新单元大小
+    for (size_t i = 0; i < ndim_value; ++i) {
+        new_dst[i] = dst_strides()[i];
+    }
+    new_dst[ndim_value] = new_unit;
+
+    // 复制源步长数据，并添加新单元大小
+    for (size_t i = 0; i < ndim_value; ++i) {
+        new_src[i] = src_strides()[i];
+    }
+    new_src[ndim_value] = new_unit;
+
+    return Result<RearrangeMeta>(layout);
+}
+
 } // namespace utils

src/utils/rearrange.h
@@ -28,6 +28,9 @@ public:
     const ptrdiff_t *src_strides() const;
 
     void launch(void *dst, const void *src) const;
+
+    // 拆分 unit 到更小的规模以利于并行
+    utils::Result<RearrangeMeta> distributeUnit(const std::vector<size_t> &candidates) const;
 };
 
 void rearrange(