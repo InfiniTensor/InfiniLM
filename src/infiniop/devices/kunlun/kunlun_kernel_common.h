0f95ad5f02ad8dbad93522a4943b9dbb66006a37
zhangyue <zhangyue@qiyuanlab.com>
2025-04-29 17:03:34 +0800

    issue/207: delete import xpu/kernel/xtdk_io.h

--- src/infiniop/devices/kunlun/kunlun_kernel_common.h
+++ src/infiniop/devices/kunlun/kunlun_kernel_common.h
@@ -4,7 +4,6 @@
 // This header file will only be include by .xpu file
 #include "kunlun_kernel_dtype.h"
 #include "xpu/kernel/xtdk.h"
-#include "xpu/kernel/xtdk_io.h"
 #include "xpu/kernel/xtdk_math.h"
 #include "xpu/kernel/xtdk_simd.h"
 #include "xpu/runtime.h"
