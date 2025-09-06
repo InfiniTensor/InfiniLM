05b4b9087f1a4cb8f1229686af201ed4c6016201
wooway777 <wooway777@gmail.com>
2025-09-06 15:00:45 +0800

    rms norm

--- src/modules/norm/rms_norm.cpp
+++ src/modules/norm/rms_norm.cpp
@@ -5,9 +5,15 @@
 namespace infinicore::nn::module {
 std::shared_ptr<RMSNorm> RMSNorm::init(size_t dim, infiniDtype_t dtype) {
     auto rms_norm = std::shared_ptr<RMSNorm>(new RMSNorm());
-
+    rms_norm->weight = Tensor::weight(nullptr, dtype, {dim});
     return rms_norm;
 }
-void RMSNorm::register_weights(infinicore::weights::Loader &loader, const std::string &name_prefix, int rank) {}
-void RMSNorm::forward(std::shared_ptr<Tensor> output, std::shared_ptr<Tensor> input) {}
+
+void RMSNorm::register_weights(infinicore::weights::Loader &loader, const std::string &name_prefix, int rank) {
+    loader.register_weight(name_prefix + ".weight", weight, rank, weights::DistributionType::FULL);
+}
+
+void RMSNorm::forward(std::shared_ptr<Tensor> output, std::shared_ptr<Tensor> input) {
+    getInferenceContext().rmsnorm(output, input, this->weight, 1e-6);
+}
 } // namespace infinicore::nn::module
