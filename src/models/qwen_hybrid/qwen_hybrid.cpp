30a7ae35bc90bbab8dada8816d1387d7c8bf79ce
PanZezhong1725 <panzezhong@qiyuanlab.com>
2025-09-12 01:58:09 +0800

    fix

--- src/models/qwen_hybrid/qwen_hybrid.cpp
+++ src/models/qwen_hybrid/qwen_hybrid.cpp
@@ -235,13 +235,14 @@ void inferDeviceBatch(const QwenHybridMeta *meta, DeviceResource &rsrc,
                                         ->slice({{0, token_offset, seq_len}, {2, 0, (key_dim * 2 + value_dim) / num_k_heads}})
                                         ->permute({1, 2, 0});
                 auto mixed_qkv_reorder = mixed_qkv->slice(1, conv_kernel_dim - 1, seq_len)->view({num_k_heads, (key_dim * 2 + value_dim) / num_k_heads, seq_len});
-                rearrange(mixed_qkv_reorder ->slice(1, 0, head_k_dim), qkvz_reorder->slice({{1, 0, head_k_dim}}));
-                rearrange(mixed_qkv_reorder ->slice(1, head_k_dim, head_k_dim), qkvz_reorder->slice(1, head_k_dim, head_k_dim));
-                rearrange(mixed_qkv_reorder ->slice(1, head_k_dim * 2, value_dim / num_k_heads), qkvz_reorder->slice(1, head_k_dim * 2, value_dim / num_k_heads));
+                rearrange(mixed_qkv_reorder->slice(1, 0, head_k_dim), qkvz_reorder->slice({{1, 0, head_k_dim}}));
+                rearrange(mixed_qkv_reorder->slice(1, head_k_dim, head_k_dim), qkvz_reorder->slice(1, head_k_dim, head_k_dim));
+                rearrange(mixed_qkv_reorder->slice(1, head_k_dim * 2, value_dim / num_k_heads), qkvz_reorder->slice(1, head_k_dim * 2, value_dim / num_k_heads));
 
                 // concat previous conv state as padding
                 rearrange(mixed_qkv->slice(1, 0, conv_kernel_dim - 1),
                           conv_state);
+                rearrange(conv_state, mixed_qkv->slice(1, seq_len, conv_kernel_dim - 1));
                 mixed_qkv = mixed_qkv->view({key_dim * 2 + value_dim, 1, conv_kernel_dim - 1 + seq_len});
                 auto mixed_qkv_out = Tensor::buffer(dt_logits, {key_dim * 2 + value_dim, 1, seq_len}, rsrc.memory_pool);
                 //---------------- conv1d
