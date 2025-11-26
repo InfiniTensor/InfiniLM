132f8c0492df4aef2ca3a17aaf9120a6b3342927
MoringLotus <lotusdurine2023@gmail.com>
2025-11-26 13:02:18 +0000

    clear enviroment

--- src/models/llada_moe/llada_moe_impl.hpp
+++ src/models/llada_moe/llada_moe_impl.hpp
@@ -25,16 +25,8 @@ struct InferRequest {
 };
 
 struct LLaDAMoEModel {
-    LLaDAMoEMeta meta;
-    infiniDevice_t device;
-    std::vector<int> dev_ids;
-    std::vector<LLaDAMoEDeviceResource> dev_resources; // # TODO: Lack of LLaDAMoEDeviceResource
-    std::vector<InferState> states;
-    std::vector<std::thread> threads;
-    InferRequest req;
-    // TODO
-    LLaDAMoEModel(const LLaDAMoEMeta *, const JiugeWeights *, infiniDevice_t device, std::vector<int> device_ids);
+
 };
 
-#include "../../cache.hpp"
+
 #endif
