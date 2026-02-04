130abdb8ec2a47a38edf0013f41f0e46b37eba9f
zhushuang <974198603@qq.com>
2026-02-04 16:00:28 +0800

    fix temp

--- python/infinilm/server/inference_server.py
+++ python/infinilm/server/inference_server.py
@@ -430,7 +430,7 @@ def main():
     elif args.metax:
         device = "cuda"
     elif args.moore:
-        device = "moore"
+        device = "musa"
     elif args.iluvatar:
         device = "cuda"
     elif args.cambricon:
