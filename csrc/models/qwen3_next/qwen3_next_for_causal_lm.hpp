ce8761d4dd084ff40a74cbf6e8844b8d8a1e89c5
qinyiqun <qinyiqun@outlook.com>
2026-08-03 05:22:31 +0000

    refactor(qwen3-next): reuse text causal LM template

--- csrc/models/qwen3_next/qwen3_next_for_causal_lm.hpp
+++ csrc/models/qwen3_next/qwen3_next_for_causal_lm.hpp
@@ -1,25 +1,21 @@
 #pragma once
 
+#include "../../layers/causal_lm_templates/text_causal_lm.hpp"
 #include "qwen3_next_decoderLayer.hpp"
+
 #include <memory>
-#include <vector>
 
 namespace infinilm::models::qwen3_next {
 
 using Qwen3NextModel = infinilm::layers::causal_lm_templates::TextModel<Qwen3NextDecoderLayer>;
 
-class Qwen3NextForCausalLM : public InfinilmModel {
+class Qwen3NextForCausalLM
+    : public infinilm::layers::causal_lm_templates::TextCausalLM<Qwen3NextModel> {
 public:
-    Qwen3NextForCausalLM(std::shared_ptr<infinilm::config::ModelConfig> model_config,
-                         const infinicore::Device &device);
-
-    Output forward(const Input &input) const override;
+    using Base = infinilm::layers::causal_lm_templates::TextCausalLM<Qwen3NextModel>;
+    using Base::Base;
 
     void reset_cache(const cache::CacheConfig *cache_config) override;
-
-protected:
-    INFINICORE_NN_MODULE(Qwen3NextModel, model);
-    INFINICORE_NN_MODULE(infinilm::layers::linear::ReplicatedLinear, lm_head);
 };
 
 std::shared_ptr<infinilm::config::ModelConfig> create_qwen3_next_model_config(std::shared_ptr<infinilm::config::ModelConfig> model_config);
