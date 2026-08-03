a05351bbc927cfa6cbca2eca24fb8c50d4b4efa9
qinyiqun <qinyiqun@outlook.com>
2026-08-03 05:15:15 +0000

    refactor(cache): centralize hybrid cache allocation

--- csrc/models/qwen3_5/qwen3_5_model.cpp
+++ csrc/models/qwen3_5/qwen3_5_model.cpp
@@ -1,7 +1,7 @@
 #include "qwen3_5_model.hpp"
 
+#include "../../cache/hybrid_cache.hpp"
 #include "../../global_state/global_state.hpp"
-#include "../qwen3_next/qwen3_next_allocate_kv_cache_tensors.hpp"
 
 #include <cstdint>
 #include <stdexcept>
@@ -115,7 +115,7 @@ void Qwen35ModelBase::reset_cache(const cache::CacheConfig *cache_config) {
 
     const backends::AttentionBackend attention_backend = infinilm::global_state::get_infinilm_config().attention_backend;
 
-    auto cache_vectors = infinilm::models::qwen3_next::qwen3_next_allocate_cache_tensors(cache_config, model_config_, attention_backend);
+    auto cache_vectors = infinilm::cache::allocate_hybrid_cache_tensors(cache_config, model_config_, attention_backend);
     forward_context.kv_cache_vec = std::move(cache_vectors.kv_cache_tensors);
     forward_context.conv_state_vec = std::move(cache_vectors.conv_state_tensors);
     forward_context.ssm_state_vec = std::move(cache_vectors.ssm_state_tensors);
