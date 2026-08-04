a6e6f3c960919ead062bb3c54c3a9799dceee36c
qinyiqun <qinyiqun@outlook.com>
2026-08-04 02:34:26 +0000

    refactor(qwen3.5): align shared MoE with model family

--- csrc/models/qwen3_next/qwen3_next_sparse_moe_block.hpp
+++ csrc/models/qwen3_next/qwen3_next_sparse_moe_block.hpp
@@ -1,9 +1,31 @@
 #pragma once
 
-#include "../../layers/moe/shared_expert_sparse_moe_block.hpp"
+#include "../../config/model_config.hpp"
+#include "../../layers/linear/linear.hpp"
+#include "../../layers/mlp/mlp.hpp"
+#include "../../layers/moe/sparse_moe_block.hpp"
+#include "infinicore/nn/module.hpp"
+
+#include <cstddef>
+#include <memory>
 
 namespace infinilm::models::qwen3_next {
 
-using Qwen3NextSparseMoeBlock = infinilm::layers::moe::SharedExpertSparseMoeBlock;
+class Qwen3NextSparseMoeBlock : public infinilm::layers::moe::SparseMoeBlock {
+public:
+    Qwen3NextSparseMoeBlock(
+        std::shared_ptr<infinilm::config::ModelConfig> model_config,
+        const infinicore::Device &device);
+    Qwen3NextSparseMoeBlock(
+        std::shared_ptr<infinilm::config::ModelConfig> model_config,
+        size_t layer_idx,
+        const infinicore::Device &device);
+
+    infinicore::Tensor forward(const infinicore::Tensor &hidden_states) const;
+
+protected:
+    INFINICORE_NN_MODULE(infinilm::layers::mlp::MLP, shared_expert);
+    INFINICORE_NN_MODULE(infinilm::layers::linear::ReplicatedLinear, shared_expert_gate);
+};
 
 } // namespace infinilm::models::qwen3_next
