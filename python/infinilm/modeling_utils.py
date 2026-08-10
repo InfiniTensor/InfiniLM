fix: ascend qw3_5 precision fix

python/infinilm/modeling_utils.py
@@ -758,7 +758,10 @@ def _remap_qwen3_5(state_dict, config):
     to_drop = []
     to_add = {}
     for key, tensor in state_dict.items():
-        if key == "model.norm.weight" or key.endswith(norm_weight_suffixes):
+        if key in (
+            "model.norm.weight",
+            "model.language_model.norm.weight",
+        ) or key.endswith(norm_weight_suffixes):
             state_dict[key] = tensor + torch.ones_like(tensor)
         elif key.endswith("linear_attn.in_proj_qkv.weight"):
             prefix = key[: -len("in_proj_qkv.weight")]